3d00be2d484780d58403731ac2b94b2bc46fbd46
Bhola Kumar <bhola.kumar@mountblue.io>
2019-02-04 12:02:20 +0530

    Change in admin.py

--- questions/admin.py
+++ questions/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from questions.models import Tag, Question, QuestionTags, Answer, Reply,\
-    Reaction, QuestionReactions
+from questions.models import Tag, Question, QuestionTag, Answer, Reply,\
+    Reaction, QuestionReaction
 
 
 # Register your models here.
 admin.site.register(Tag)
 admin.site.register(Question)
-admin.site.register(QuestionTags)
+admin.site.register(QuestionTag)
 admin.site.register(Answer)
 admin.site.register(Reply)
 admin.site.register(Reaction)
-admin.site.register(QuestionReactions)
+admin.site.register(QuestionReaction)
