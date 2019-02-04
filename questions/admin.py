936115bbbbb7874e497a93ea54071c5475576e28
Bhola Kumar <bhola.kumar@mountblue.io>
2019-02-04 10:54:49 +0530

    Register all models to admin

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from questions.models import Tag, Question, QuestionTags, Answer, Reply,\
+    Reaction, QuestionReactions
+
 
 # Register your models here.
+admin.site.register(Tag)
+admin.site.register(Question)
+admin.site.register(QuestionTags)
+admin.site.register(Answer)
+admin.site.register(Reply)
+admin.site.register(Reaction)
+admin.site.register(QuestionReactions)
