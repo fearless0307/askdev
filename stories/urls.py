d66b222c39e19dc4b008598228532e3c5b176dae
Bhola Kumar <bhola.kumar@mountblue.io>
2019-02-04 19:01:34 +0530

    Stories urls.py changes

--- stories/urls.py
+++ stories/urls.py
@@ -1,16 +1,12 @@
 from django.urls import path
 from stories import views
 
-
-urlpatterns = [
-    path('api/story/',views.Story_list.as_view()),
-    path('api/story/<int:pk>/',views.Story_detail.as_view()),
-    path('api/storytags/',views.StoryTag_list.as_view()),
-    path('api/storytags/<int:pk>/',views.StoryTag_detail.as_view()),
-    path('api/storyreaction/',views.StoryReaction_list.as_view()),
-    path('api/storyreaction/<int:pk>',views.StoryReaction_detail.as_view()),
-
-]
 urlpatterns = [
     path('', views.stories_home, name='stories-home'),
+    path('api/story/', views.Story_list.as_view()),
+    path('api/story/<int:pk>/', views.Story_detail.as_view()),
+    path('api/storytags/', views.StoryTag_list.as_view()),
+    path('api/storytags/<int:pk>/', views.StoryTag_detail.as_view()),
+    path('api/storyreaction/', views.StoryReaction_list.as_view()),
+    path('api/storyreaction/<int:pk>', views.StoryReaction_detail.as_view()),
 ]
